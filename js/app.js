70949111e91a299361d8fc37e4e8ca71dcf63968
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-04-01 11:18:26 -0700

    Added eslintrc file

--- js/app.js
+++ js/app.js
@@ -23,24 +23,22 @@ Project.prototype.toHtml = function() {
 
 
 
-    $newProject.find('h1').html(this.name);
-    $newProject.find('.description').html(this.description);
-    $newProject.find('a').html('Hover over this area to see a preview. Click to go to the site.');
-    $newProject.find('a').attr('href', this.url);
-    $newProject.find('time').attr('datetime', this.date);
-    console.log('fin: ', $newProject);
+  $newProject.find('h1').html(this.name);
+  $newProject.find('.description').html(this.description);
+  $newProject.find('a').html('Hover over this area to see a preview. Click to go to the site.');
+  $newProject.find('a').attr('href', this.url);
+  $newProject.find('time').attr('datetime', this.date);
+  console.log('fin: ', $newProject);
 
 
   return $newProject;
 };
 
 rawData.sort(function(a,b) {
-  // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
   return (new Date(b.date)) - (new Date(a.date));
 });
 
 rawData.forEach(function(projectObject) {
-  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
   projectsArray.push(new Project(projectObject));
 });
 
