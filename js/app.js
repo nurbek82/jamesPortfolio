bbbf69ae76f17c7db022707d0cb1a575e8fd0439
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-31 20:03:01 -0700

    added jquery to app.js but it is not working

--- js/app.js
+++ js/app.js
@@ -5,50 +5,49 @@ var projectsArray = [];
 function Projects(opts){
   this.name = opts.name;
   this.description = opts.description;
-  this.url = opt.url;
-  this.date = opt.date
+  this.url = opts.url;
+  this.date = opts.date
   projectsArray.push(this);
 };
 
-// Projects.prototype.toHtml = function() {
-//   var $newProject = $('section.template').clone();
-//
-//
-//   $newArticle.removeClass('template');
-//
-//   if (!this.publishedOn) $newArticle.addClass('draft');
-//   $newArticle.data('category', this.category);
-//
-//
-//
-//     $newArticle.findClass('protitle').html(this.name);
-//     $newArticle.find('a').html(this.author);
-//     $newArticle.find('a').attr('href', this.authorUrl);
-//     $newArticle.find('time').attr('datetime', this.publishedOn);
-//     $newArticle.find('.article-body').html(this.body);
-//     console.log($newArticle);
-//
-//   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-//   $newArticle.append('<hr>');
-//   return $newArticle;
-// };
-//
-// rawData.sort(function(a,b) {
-//   // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
-//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-// });
-//
-// rawData.forEach(function(articleObject) {
-//   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
-//   articles.push(new Article(articleObject));
-// });
-//
-// articles.forEach(function(a) {
-//   $('#articles').append(a.toHtml());
-// });
+Projects.prototype.toHtml = function() {
+  var $newProject = $('section.template').clone();
 
-$('#livepreview').urlive().hover(function(){
-    $(this).urlive('open');
-},function(){
-    $(this).urlive('close');
+
+  $newProject.removeClass('template');
+
+  if (!this.date) $newProject.addClass('draft');
+  $newProject.data('category', this.category);
+
+
+
+    $newProject.find('.h1-protitle').html(this.name);
+    $newProject.find('.p-description').html(this.description);
+    $newProject.find('a').attr('href', this.url);
+    $newProject.find('time').attr('datetime', this.date);
+    $newProject.find('.article-body').html(this.body);
+    console.log($newProject);
+
+  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.date))/60/60/24/1000) + ' days ago');
+  $newProject.append('<hr>');
+  return $newProject;
+};
+
+rawData.sort(function(a,b) {
+  // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
+  return (new Date(b.date)) - (new Date(a.date));
 });
+
+rawData.forEach(function(projectObject) {
+  // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+  projectsArray.push(new Projects(projectObject));
+  console.log(projectObject);
+});
+
+projectsArray.forEach(function(a) {
+  $('#projectsArray').append(a.toHtml());
+});
+
+// $(document).ready(function() {
+//   $(".livepreview").livePreview();
+// });
---
+++ js/projectobjects.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var rawData = [
+
+  {
+    name:   'Guessing Game',
+    url: 'https://jamesbillard12.github.io/gues…',
+    date: '2017-11-05',
+    description: 'The first page I ever made',
+
+  },
+
+];
