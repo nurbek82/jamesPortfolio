ec6763c24dcfc53c2df88e4e35b8d5098bd6ed00
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-31 18:43:17 -0700

    added jquery plug for preview of project  image

--- js/app.js
+++ js/app.js
@@ -2,9 +2,53 @@
 var projectsArray = [];
 
 
-function Projects(name, description, url){
-  this.name = name;
-  this.description = description;
-  this.url = url;
+function Projects(opts){
+  this.name = opts.name;
+  this.description = opts.description;
+  this.url = opt.url;
+  this.date = opt.date
   projectsArray.push(this);
-}
+};
+
+// Projects.prototype.toHtml = function() {
+//   var $newProject = $('section.template').clone();
+//
+//
+//   $newArticle.removeClass('template');
+//
+//   if (!this.publishedOn) $newArticle.addClass('draft');
+//   $newArticle.data('category', this.category);
+//
+//
+//
+//     $newArticle.findClass('protitle').html(this.name);
+//     $newArticle.find('a').html(this.author);
+//     $newArticle.find('a').attr('href', this.authorUrl);
+//     $newArticle.find('time').attr('datetime', this.publishedOn);
+//     $newArticle.find('.article-body').html(this.body);
+//     console.log($newArticle);
+//
+//   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+//   $newArticle.append('<hr>');
+//   return $newArticle;
+// };
+//
+// rawData.sort(function(a,b) {
+//   // REVIEW: Take a look at this sort method; This may be the first time we've seen it.
+//   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+// });
+//
+// rawData.forEach(function(articleObject) {
+//   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
+//   articles.push(new Article(articleObject));
+// });
+//
+// articles.forEach(function(a) {
+//   $('#articles').append(a.toHtml());
+// });
+
+$('#livepreview').urlive().hover(function(){
+    $(this).urlive('open');
+},function(){
+    $(this).urlive('close');
+});
