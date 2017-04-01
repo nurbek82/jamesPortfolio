7bb4539830ee88bdc58bff2b2c7a8cef3847e656
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-03-31 21:23:04 -0700

    finally got information to populate to dom with out doubles. I was pushing it twice.

--- js/app.js
+++ js/app.js
@@ -8,15 +8,15 @@ function Project(opts){
   this.url = opts.url;
   this.date = opts.date;
   this.category = opts.category;
-  projectsArray.push(this);
 }
 
 Project.prototype.toHtml = function() {
   var $newProject = $('section.template').clone();
-
+  console.log('go: ', $newProject);
 
   $newProject.removeClass('template');
-  console.log($newProject);
+  console.log('re: ', $newProject);
+
 
   if (!this.date) $newProject.addClass('draft');
   $newProject.data('category', this.category);
@@ -27,7 +27,7 @@ Project.prototype.toHtml = function() {
     $newProject.find('.description').html(this.description);
     $newProject.find('a').attr('href', this.url);
     $newProject.find('time').attr('datetime', this.date);
-    console.log($newProject);
+    console.log('fin: ', $newProject);
 
   $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.date))/60/60/24/1000) + ' days ago');
   $newProject.append('<hr>');
@@ -42,13 +42,12 @@ rawData.sort(function(a,b) {
 rawData.forEach(function(projectObject) {
   // REVIEW: Take a look at this forEach method; This may be the first time we've seen it.
   projectsArray.push(new Project(projectObject));
-  console.log(projectObject);
 });
 
 projectsArray.forEach(function(a) {
   $('#projectstodom').append(a.toHtml());
 });
 
-// $(document).ready(function() {
-//   $(".livepreview").livePreview();
-// });
+$(document).ready(function() {
+  $(".livepreview").livePreview();
+});
--- js/projectobjects.js
+++ js/projectobjects.js
@@ -4,10 +4,17 @@ var rawData = [
 
   {
     name:   'Guessing Game',
-    url: 'https://jamesbillard12.github.io/gues…',
+    url: 'https://jamesbillard12.github.io/guessing_game/',
     date: '2017-11-03',
     description: 'The first page I ever made at Code Fellows.',
 
+  },
+
+  {
+    name:   'Salmon Cookies',
+    url: 'https://jamesbillard12.github.io/guessing_game/',
+    date: '2017-11-03',
+    description: 'The first page I ever made at Code Fellows.',
   },
 
 ];
