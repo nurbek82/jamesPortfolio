9b14155e691d6e2bed03c167d2ca9fc601b90903
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-04-08 15:34:05 -0700

    took away some unneeded code

--- js/app.js
+++ js/app.js
@@ -11,21 +11,10 @@ function Project(rawDataObj){
 }
 
 Project.prototype.toHtml = function() {
-  $('section.template').hide('.template');
   var source = $('#template').html();
   var template = Handlebars.compile(source);
-
-
-
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
-
   return template(this);
 };
-
-rawData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
 
 rawData.forEach(function(projectObject) {
   projectsArray.push(new Project(projectObject));
