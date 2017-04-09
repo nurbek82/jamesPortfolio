f18be490d85137f94ff37444339f9819349ccbc9
James Billard <jamesbillard@Jamess-iMac.local>
2017-04-09 13:54:30 -0700

    Fuctionality work for about resume I've got some work ahead of me to style this

---
+++ js/aboutApp.js
@@ -0,0 +1,28 @@
+'use strict';
+var resumeArray = [];
+
+
+function Resume(aboutRawDataObj){
+  this.company = aboutRawDataObj.company;
+  this.position= aboutRawDataObj.position
+  this.cityPlace = aboutRawDataObj.cityPlace
+  this.employmentPeriodStart = aboutRawDataObj.employmentPeriodStart;
+  this.employmentPeriodEnd = aboutRawDataObj.employmentPeriodEnd;
+  this.description = aboutRawDataObj.description;
+  this.acategory = aboutRawDataObj.acategory;
+}
+
+
+Resume.prototype.aboutToHtml = function() {
+  var aboutSource = $('#aboutTemplate').html();
+  var aboutTemplate = Handlebars.compile(aboutSource);
+  console.log('poop ' +aboutTemplate);
+  return aboutTemplate(this);
+};
+aboutRawData.forEach(function(aboutObject) {
+  resumeArray.push(new Resume(aboutObject));
+});
+
+resumeArray.forEach(function(about) {
+  $('#abouttodom').append(about.aboutToHtml());
+});
---
+++ js/projectApp.js
@@ -0,0 +1,26 @@
+
+'use strict';
+var projectsArray = [];
+
+
+function Project(rawDataObj){
+  this.name = rawDataObj.name;
+  this.description = rawDataObj.description;
+  this.url = rawDataObj.url;
+  this.date = rawDataObj.date;
+  this.category = rawDataObj.category;
+}
+
+Project.prototype.toHtml = function() {
+  var source = $('#template').html();
+  var template = Handlebars.compile(source);
+  return template(this);
+};
+
+rawData.forEach(function(projectObject) {
+  projectsArray.push(new Project(projectObject));
+});
+
+projectsArray.forEach(function(pjects) {
+  $('#projectstodom').append(pjects.toHtml());
+});
