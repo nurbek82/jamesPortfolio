85fcefc0c5db02a9c84d9743f0ec58c977f75675
jamesbillard <jamesbillard12@hotmail.com>
2017-04-09 23:15:26 -0700

    added first year in northen lights to aboutobjects

--- js/aboutObjects.js
+++ js/aboutObjects.js
@@ -1,15 +1,24 @@
 'use strict';
 
 var aboutRawData = [
+  {
+    company:'Northern Lights School Division #113',
+    cityPlace: 'La Ronge, Saskatchewan',
+    position: 'Churchill Community High School, La Ronge, Saskatchewan, Satellite Teacher (English 8, 9 and 11, Core French 7-9, Wellness 10 French Immersion and English)',
+    employmentPeriodStart: '2012',
+    employmentPeriodEnd: '2013',
+    description: 'Provided students with constructive and positive feedback to foster intrinsic motivation. Presented ideas clearly and concisely using multiple platforms such as guest speakers, field trips, videos, Power Point presentations, small group work, and one-on-one mentorship ',
+    acategory: 'Work Experience',
+  },
+
  {
    company:'Yukon Government',
    cityPlace: 'Watson Lake, Yukon',
-   position: 'French Teacher',
-   employmentPeriodStart: '2010',
+   position: 'Core French grades 1-7, Music, Technology, Health, and Physical Education)',
+   employmentPeriodStart: '2009',
    employmentPeriodEnd: '2012',
-   description: 'Worked as a French Teacher',
+   description: 'Designed hands-on education experiences for students which cultivated curiosity, creativity, and critical thinking skills. Took a leadership role in school wide activities; organized school wide functions. Modified instruction for English Language Learners and students on individual education programs by using various intervention techniques as well as presented information engaging to auditory, visual, and kinaesthetic learners.',
    acategory: 'Work Experience',
-
  },
 
 ];
