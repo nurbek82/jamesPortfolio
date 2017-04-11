4ee6b561cb6c964d6fc9aae76b2cec90868afdd0
jamesbillard <jamesbillard12@hotmail.com>
2017-04-11 14:23:45 -0700

    Added some video game like 80s animations to the about page

--- js/aboutView.js
+++ js/aboutView.js
@@ -21,15 +21,15 @@ aboutView.aboutHandleCategoryFilter = function() {
       console.log('happening');
 
 
-      $('section.abox').fadeOut('slow');
+      $('section.abox').hide('pulsate');
 
-      $('section[data-category="' + $(this).val() + '"]').delay(800).fadeIn('slow');
-
+      $('section[data-category="' + $(this).val() + '"]').animate({opacity:1}).delay(200).slideDown('slow');
+      $('section[data-category="' + $(this).val() + '"]').effect( "bounce" );
       var $newArticle = ($(this).val());
       console.log($newArticle);
     } else {
 
-      $('section.abox').fadeIn();
+      $('section.abox').animate({opacity:1}).delay(200).slideDown('slow');
       $('article.template').hide();
     }
     $('#author-filter').val('');
@@ -39,36 +39,36 @@ aboutView.aboutHandleCategoryFilter = function() {
 // aboutView.handleMainNav = function () {
 //
 //
-//   $('.main-nav').on('click', ‘.tab', function() {
+//   $('.main-nav').on('click', '.tab', function() {
 //
 //     event.preventDefault();
-//     $(‘section.tab-content').hide();
+//     $('section.tab-content').hide();
 //     // 2.
-//     $(‘#' + $(this).attr(‘data-content')).fadeIn();
+//     $('#' + $(this).attr('data-content')).fadeIn();
 //
 //   });
 // };
 
 // articleView.setTeasers = function() {
 //   // Truncate logic to show only first two elements within the article body.
-//   $(‘.article-body *:nth-of-type(n+2)').hide();
-//   $(‘article').on(‘click', ‘.read-on', function(){
-//     console.log(‘event');
+//   $('.article-body *:nth-of-type(n+2)').hide();
+//   $('article').on('click', '.read-on', function(){
+//     console.log('event');
 //     event.preventDefault();
-//     $(this).siblings(‘section.article-body').children().toggle();
+//     $(this).siblings('section.article-body').children().toggle();
 //
-//     if ($(this).html() === ‘Read on →') {
-//       $(this).html(‘Read less &larr;');
+//     if ($(this).html() === 'Read on →') {
+//       $(this).html('Read less &larr;');
 //     }
 //     else {
-//       $(this).html(‘Read on →');
+//       $(this).html('Read on →');
 //     }
 //   });
 $(function(){
 
 
-   aboutView.aboutPopulateFilter();
-   aboutView.aboutHandleCategoryFilter();
-  //  aboutView.handleMainNav();
- // articleView.setTeasers();
+    aboutView.aboutPopulateFilter();
+    aboutView.aboutHandleCategoryFilter();
+    aboutView.handleMainNav();
+  // articleView.setTeasers();
 });
