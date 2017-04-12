6de706a4e44c4acc4c249e5b604c40e66dcb8ebb
jamesbillard <jamesbillard12@hotmail.com>
2017-04-12 00:30:16 -0700

    I am able to get an object through ajax but I can't figure out the handlebars bit again

--- js/aboutApp.js
+++ js/aboutApp.js
@@ -1,27 +1,48 @@
-'use strict';
+
+
 var resumeArray = [];
 
-
 function Resume(aboutRawDataObj){
- this.company = aboutRawDataObj.company;
- this.position= aboutRawDataObj.position
- this.cityPlace = aboutRawDataObj.cityPlace
- this.employmentPeriodStart = aboutRawDataObj.employmentPeriodStart;
- this.employmentPeriodEnd = aboutRawDataObj.employmentPeriodEnd;
- this.description = aboutRawDataObj.description;
- this.acategory = aboutRawDataObj.acategory;
+  this.company = aboutRawDataObj.company;
+  this.position= aboutRawDataObj.position
+  this.cityPlace = aboutRawDataObj.cityPlace
+  this.employmentPeriodStart = aboutRawDataObj.employmentPeriodStart;
+  this.employmentPeriodEnd = aboutRawDataObj.employmentPeriodEnd;
+  this.description = aboutRawDataObj.description;
+  this.acategory = aboutRawDataObj.acategory;
 }
 
 
 Resume.prototype.aboutToHtml = function() {
- var aboutSource = $('#aboutTemplate').html();
- var aboutTemplate = Handlebars.compile(aboutSource);
- return aboutTemplate(this);
+  var aboutSource = $('#aboutTemplate').html();
+  var aboutTemplate = Handlebars.compile(aboutSource);
+  return aboutTemplate(this);
 };
-aboutRawData.forEach(function(aboutObject) {
- resumeArray.push(new Resume(aboutObject));
-});
+
+
+
+
+// aboutRawData.forEach(function(aboutObject) {
+//  resumeArray.push(new Resume(aboutObject));
+// });
 
 resumeArray.forEach(function(about) {
- $('#abouttodom').append(about.aboutToHtml());
+  $('#abouttodom').append(about.aboutToHtml());
+});
+
+$(function(){
+  $.ajax({
+    url: '/js/aboutObjects.json',
+    dataType : "json",
+  }).done(function(data) {
+    console.log(data);
+    console.log('request done: ' + new Date());
+    data.forEach(function(aboutObject) {
+      resumeArray.push(new Resume(aboutObject));
+    });
+  }).fail(function(xhr, status, error){
+    console.log("Status: " + status + " Error: " + error);
+    console.log(xhr);
+});
+  console.log('request started: ' + new Date());
 });
