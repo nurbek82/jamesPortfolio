bd85f6b291e20e8a1d94e06a4ecac84ce2a24c11
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-04-14 00:34:06 -0700

    cleaned up app files deleted commented out code

--- js/aboutApp.js
+++ js/aboutApp.js
@@ -54,24 +54,3 @@ if (localStorage.aboutRawData) {
     })
   })
 }
-
-
-// $(function(){
-//   $.ajax({
-//     url: '/js/aboutObjects.json',
-//     dataType : "json",
-//   }).done(function(data) {
-//     console.log(data);
-//     console.log('request done: ' + new Date());
-//     data.forEach(function(aboutObject) {
-//       resumeArray.push(new Resume(aboutObject));
-//     });
-//     resumeArray.forEach(function(about) {
-//       $('#abouttodom').append(about.aboutToHtml());
-//     });
-//     aboutView.aboutPopulateFilter();
-//     aboutView.aboutHandleCategoryFilter();
-//     aboutView.setTeasers();
-//   })
-//   console.log('request started: ' + new Date());
-// });
