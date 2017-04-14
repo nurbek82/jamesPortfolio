5774ed6ac2acd91c6d0b8e966c577d7f06fee769
jamesbillard <jamesbillard12@hotmail.com>
2017-04-13 23:56:08 -0700

    Added local Storage to ajax request on musicapp js. the data is being stored as JSON

--- js/musicapp.js
+++ js/musicapp.js
@@ -16,20 +16,30 @@ Music.prototype.musicToHtml = function() {
  return musicTemplate(this);
 };
 
-
-$(function(){
-  $.ajax({
-    url: '/js/musicobjects.json',
-    dataType : "json",
-  }).done(function(data) {
-    console.log(data);
-    console.log('request done: ' + new Date());
-    data.forEach(function(musicObject) {
-      musicArray.push(new Music(musicObject));
-    });
-    musicArray.forEach(function(music) {
-      $('#musictodom').append(music.musicToHtml());
-    });
-  })
-  console.log('request started: ' + new Date());
-});
+if (localStorage.musicRawData) {
+  var music = JSON.parse(localStorage.musicRawData);
+  music.forEach(function(musicObject) {
+    musicArray.push(new Music(musicObject));
+  });
+  musicArray.forEach(function(music) {
+    $('#musictodom').append(music.musicToHtml());
+  });
+  console.log("BALLS");
+}else {
+  console.log("COCK");
+  $(function(){
+    $.ajax({
+      url: '/js/musicobjects.json',
+      dataType : "json",
+    }).done(function(data) {
+      localStorage.setItem('musicRawData', JSON.stringify(data));
+      var music = JSON.parse(localStorage.musicRawData);
+      music.forEach(function(musicObject) {
+        musicArray.push(new Music(musicObject));
+      });
+      musicArray.forEach(function(music) {
+        $('#musictodom').append(music.musicToHtml());
+      });
+    })
+  });
+}
